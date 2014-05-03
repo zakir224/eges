22db63a2a8f14a371ad3b9f712f2c848b752b9a7
tumpa <tumpa.fci@gmail.com>
2014-05-04 01:05:43 +0600

    dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -29,7 +29,82 @@
             <div class="col-lg-12 well"  style="background: #ffffff;">
 
                 <div class="col-lg-12">
-                    <h3>List of Applicants</h3>
+                    <h3 style="font-size: xx-large">Dashboard</h3>
+                    <hr>
+                    <h4>Personal Files/Attachments</h4>
+                    <div class="row">
+
+                        <div class="col-lg-4">
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> Passport
+                            </label>
+                        </div>
+                    </div>
+                    <h4>Educational Files/Attachments</h4>
+                    <div class="row">
+                        <div class="col-lg-4">
+
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox2" value="option2"> SSC Certificate
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox3" value="option3"> SSC Marks Sheet
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox2" value="option2"> HSC Certificate
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox3" value="option3"> HSC Marks Sheet
+                            </label>
+                        </div>
+                        <div class="col-lg-4">
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> Bachelors Certificate
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox2" value="option2"> Bachelors Marks Sheet
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox3" value="option3"> Masters Certificate
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> Masters Marks Sheet
+                            </label>
+                        </div>
+                        <div class="col-lg-4">
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> Other Papers
+                            </label>
+                        </div>
+                    </div>
+                    <h4>Financial Files/Attachments</h4>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> 1
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox2" value="option2"> 2
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox3" value="option3"> 3
+                            </label>
+                        </div>
+                        <div class="col-lg-4">
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> 1
+                            </label>
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox2" value="option2"> 2
+                            </label>
+                        </div>
+                        <div class="col-lg-4">
+                            <label class="checkbox">
+                                <input type="checkbox" id="inlineCheckbox1" value="option1"> Other Files
+                            </label>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
@@ -38,27 +113,7 @@
 </section>
 
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/tooltip.js"></script>
-<script src="js/tooltrip.js"></script>
 <!-- toolTip JS -->
-<script type="text/javascript">
-    $('[data-toggle="tooltip"]').tooltip({
-        'placement': 'top'
-    });
-    $('[data-toggle="popover"]').popover({
-        trigger: 'hover',
-        'placement': 'top'
-    });
-
-    $('#userNameField').tooltip({
-        'show': true,
-        'placement': 'bottom',
-        'title': "Please remember to..."
-    });
-
-    $('#userNameField').tooltip('show');
-
-</script>
 <!-- End ToolTip JS -->
 
 <script src="js/bootstrap.min.js"></script>
