364c76149e66dadba9f4a26b6066725aeb458557
tumpa <tumpa.fci@gmail.com>
2014-05-19 01:19:35 +0600

    cv

--- application/views/applicant_cv.php
+++ application/views/applicant_cv.php
@@ -28,66 +28,82 @@
 
         <div class="col-lg-12 well"  style="background: #ffffff;">
 
-            <div class="col-lg-12">
-                <ul class="list-unstyled col-lg-8" >
-                    <h4>Personal Information</h4>
+            <div class="col-lg-9">
+                <ul class="list-unstyled col-lg-12" >
+                    &nbsp;
 
-                    <li><span>User Name:</span> <?php echo "Shamima Nasrin";?></li>
+                    <h4>Personal Information</h4>
+                    <hr>
+                    <li><span>First Name:</span> <?php echo "afdsfsas";?></li>
                     &nbsp;
-                    <li><span>Counrty:</span> Bangladesh</li>
+                    <li style=""><span>Last Name:</span> <?php echo "afdsfsas";?></li>
                     &nbsp;
-                    <li><span>Birthday:</span> <?php echo "12/03/1991";?></li>
+                    <li><span>Father's Name:</span> <?php echo "afdsfsas";?></li>
                     &nbsp;
-                    <li><span>Occupation:</span> Web Developer</li>
+                    <li><span>Mother's Name:</span> <?php echo "afdsfsas";?></li>
                     &nbsp;
-                    <li><span>Email:</span> <a href="#"><?php echo "tumpa.ffsda@mail.com";?></a></li>
+                    <li><span>Date Of Birth:</span> <?php echo "00/11/2000";?></li>
                     &nbsp;
-                    <li><span>Interests:</span> Cycling, Music, Anling</li>
+                    <li><span>Present Address:</span> dhanmondi,dhaka-1200,Bangladesh</li>
                     &nbsp;
-                    <li><span>Website Url:</span> <a href="#">http://www.mywebsite.com</a></li>
+                    <li><span>Permanent Address:</span> dhanmondi,dhaka-1200,Bangladesh</li>
                     &nbsp;
-                    <li><span>Mobile Number:</span></li>
+                    <li><span>E-mail:</span> <a href="#">jhgj@gmail.com</a></li>
+                    &nbsp;
+                    <li><span>Mobile Number:</span>+880-1812345678</li>
                     &nbsp;
                     <li>
                         <h4>Educational Information</h4>
+                        <hr>
                         <table class="table table-striped" border="1" width="100%" style="text-align: center">
                             <tr>
-                                <th>Education Name</th>
-                                <th>Result</th>
-                                <th>Institute Name</th>
-                                <th>Passing Year</th>
+                                <th width="25%" style="text-align: center">Education Name</th>
+                                <th width="15%" style="text-align: center">Result</th>
+                                <th style="text-align: center">Institute Name</th>
+                                <th width="15%" style="text-align: center">Passing Year</th>
                             </tr>
 
                             <tr>
-                                <td width="20%">SSC</td>
-                                <td width="20%">5.00</td>
+                                <td >Secondary/Equivalent level</td>
+                                <td >5.00</td>
                                 <td>Feni Govt. Pilot High School</td>
-                                <td width="20%">2007</td>
+                                <td >2007</td>
 
                             </tr>
                             <tr>
-                                <td width="20%">HSC</td>
-                                <td width="20%">4.90</td>
+                                <td>Intermediate/equivalent level</td>
+                                <td >4.90</td>
                                 <td>Bir Shreshtha Noor Mohammad Rifle Public School and College</td>
-                                <td width="20%">2009</td>
+                                <td >2009</td>
 
                             </tr>
 
                             <tr>
-                                <td width="20%">BSCSE</td>
-                                <td width="20%">3.85</td>
+                                <td >Bachelors</td>
+                                <td >3.85</td>
                                 <td>United International University</td>
-                                <td width="20%">2014</td>
+                                <td >2014</td>
+
+                            </tr>
+                            <tr>
+                                <td >Masters</td>
+                                <td >3.85</td>
+                                <td>United International University</td>
+                                <td>2014</td>
 
                             </tr>
 
                         </table>
                     </li>
-
-
                 </ul>
 
 
+
+            </div>
+            <div class="col-lg-3">
+                <a href="" class="">
+                    <img src="images/default_image.png" width="150px" alt="web image">
+                </a>
             </div>
 
         </div>
